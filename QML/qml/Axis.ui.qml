71436bcba0ed8738f0105d4cbfa039088ab57955
Hansen0314 <595355940@qq.com>
2021-03-12 11:20:12 +0000

    Add qml base code

---
+++ QML/qml/Axis.ui.qml
@@ -0,0 +1,87 @@
+import QtQuick 2.8
+import QtCharts 2.2
+
+Item {
+    id: axis
+    width: 850
+    height: 490
+
+    property alias axisX: axis_x
+    property alias axisY: axis_y
+    property alias axisZ: axis_z
+
+    Image {
+        id: rectangle_8_copy_3
+        x: 1
+        y: 1
+        source: "assets/rectangle_8_copy_3.png"
+    }
+
+    Text {
+        id: axis_accelerometer
+        x: 43
+        y: 40
+        width: 183
+        height: 21
+        color: "#000000"
+        text: "3-Axis Accelerometer"
+        font.pixelSize: 21
+        horizontalAlignment: Text.AlignLeft
+        font.family: "Microsoft YaHei"
+        font.styleName: "Regular"
+    }
+
+    ChartView {
+        id: chartview
+        title: "Accelerator 3-Axis"
+        // title.Alignment: Qt.AlignLeft
+        anchors.fill: parent
+        antialiasing: true
+
+        //X轴
+        ValueAxis {
+            id: valueAxisX
+            min: 0
+            max: 100
+            tickCount: 10
+        }
+
+        //Y轴
+        ValueAxis {
+            id: valueAxisY
+            min: 0
+            max: 100
+            tickCount: 10
+        }
+
+        SplineSeries {
+            id:axis_x
+            name: "x"
+            axisX: valueAxisX
+            axisY: valueAxisY
+        }
+
+        SplineSeries {
+            id:axis_y
+            name: "y"
+            axisX: valueAxisX
+            axisY: valueAxisY
+        }
+
+        SplineSeries {
+            id:axis_z
+            name: "z"
+            axisX: valueAxisX
+            axisY: valueAxisY
+            
+        }
+    }
+}
+
+/*##^##
+Designer {
+    D{i:0;uuid:"2ccdf9d4e81bbab5cc412a7c11f0a703"}D{i:1;uuid:"7e30687bb86308fdba6bdd88447383b8"}
+D{i:2;uuid:"938efa451ca78a23a21f36216a2709ad"}
+}
+##^##*/
+
---
+++ QML/qml/main.py
@@ -0,0 +1,105 @@
+# This Python file uses the following encoding: utf-8
+import os, random
+import sys
+import PySide2.QtQml
+from PySide2.QtWidgets import *
+from PySide2.QtQuick import *
+from PySide2.QtCore import *
+
+#定时器的方式
+# class MyClass(QObject):
+#     timerSignal = Signal(int)
+
+#     def __init__(self):
+#         super().__init__()
+
+#         # 定义一个时间信号
+        
+#         self._timer = QTimer(self, timeout=self.onTimeout)
+#         self._timer.start(1000)
+
+#     def onTimeout(self):
+#         # 定时器发送信号通知qml
+#         val = random.randint(1,100)
+#         print("val",val)
+#         self.timerSignal.emit(int(val))
+
+
+# 线程的方式
+class CpuUsage(QThread):
+    CpuSignal = Signal(int)
+    def __init__(self):
+        super().__init__()
+    
+    def run(self):
+        while True:
+            val = random.randint(1,100)
+            print("vvv1",val)
+            self.CpuSignal.emit(int(val))
+            self.sleep(1)
+
+
+class RamUsage(QThread):
+    RamSignal = Signal(int)
+    def __init__(self):
+        super().__init__()
+    
+    def run(self):
+        while True:
+            val = random.randint(1,100)
+            print("vvv2",val)
+            self.RamSignal.emit(int(val))
+            self.sleep(1)
+
+
+class StorageUsage(QThread):
+    StoragSignal = Signal(int)
+    def __init__(self):
+        super().__init__()
+    
+    def run(self):
+        while True:
+            val = random.randint(1,100)
+            print("vvv3",val)
+            self.StoragSignal.emit(int(val))
+            self.sleep(1)
+
+
+class Accelerator(QThread):
+    AxisSignal = Signal(int,int,int)
+    def __init__(self):
+        super().__init__()
+    
+    def run(self):
+    
+        while True:
+            val1 = random.randint(1,100)
+            val2 = random.randint(1,100)
+            val3 = random.randint(1,100)
+            self.AxisSignal.emit(val1,val2,val3)
+            self.sleep(1)
+
+
+if __name__ == '__main__':
+    app = QApplication([])
+    view = QQuickView()
+    url = QUrl("Ui.ui.qml")
+
+    cpu = CpuUsage()
+    arm = RamUsage()
+    Storage = StorageUsage()
+    axis = Accelerator()
+
+    context = view.rootContext()
+    context.setContextProperty("_CpuUsage", cpu)
+    context.setContextProperty("_RamUsage", arm)
+    context.setContextProperty("_StorageUsage", Storage)
+    context.setContextProperty("_Accelerator", axis)
+    cpu.start()
+    arm.start()
+    Storage.start()
+    axis.start()
+
+    view.setSource(url)
+    view.show()
+    app.exec_()
